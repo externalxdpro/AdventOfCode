5e05508bd7805bc831d50c7923a0a2f69555007c
externalxdpro <externalxdpro@gmail.com>
2024-12-04 18:32:49 -0500

    Add day 4

---
+++ 2024/Haskell/Day 4/main.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Main where
+
+import Control.Arrow ((&&&))
+import Data.List (transpose)
+
+type Input = [String]
+
+diagonals :: [String] -> [String]
+diagonals [] = []
+diagonals ([] : xss) = xss
+diagonals xss =
+  zipWith
+    (++)
+    (map ((: []) . head) xss ++ repeat [])
+    ([] : diagonals (map tail xss))
+
+count :: String -> Int
+count [] = 0
+count ('X' : 'M' : 'A' : 'S' : xs) = 1 + count xs
+count (_ : xs) = count xs
+
+submatrices :: Int -> [String] -> [[String]]
+submatrices n =
+  map transpose . concatMap (groups n . transpose) . groups n
+  where
+    groups n xs = [take n $ drop i xs | i <- [0 .. length xs - n]]
+
+isCross :: [String] -> Bool
+isCross = any cross . sequence [id, transpose, reverse, reverse . transpose]
+  where
+    cross [['M', _, 'M'], [_, 'A', _], ['S', _, 'S']] = True
+    cross _ = False
+
+part1 :: Input -> Int
+part1 xs =
+  let lrDiag = diagonals xs
+      rlDiag = diagonals (map reverse xs)
+      allDirs = concat [lrDiag, rlDiag, xs, transpose xs]
+      counts = map count allDirs ++ map (count . reverse) allDirs
+   in sum counts
+
+part2 :: Input -> Int
+part2 = length . filter id . map isCross . submatrices 3
+
+prepare :: String -> Input
+prepare = lines
+
+main :: IO ()
+main = do
+  readFile "small1.txt" >>= print . part1 . prepare
+  readFile "small2.txt" >>= print . part2 . prepare
+  readFile "input.txt" >>= print . (part1 &&& part2) . prepare
