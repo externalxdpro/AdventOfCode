91a6a5bae8bb3af79fd62c13008a9695a32c16fc
externalxdpro <externalxdpro@gmail.com>
2024-12-09 12:27:56 -0500

    Add formatted solution

--- 2024/Haskell/Day 7/main.hs
+++ 2024/Haskell/Day 7/main.hs
@@ -4,35 +4,36 @@
 module Main where
 
 import Control.Arrow ((&&&))
-import Data.List.Split (splitOn)
 import Data.Bifunctor (second)
+import Data.List.Split (splitOn)
 
 type Input = [String]
 
 calibrate :: [Int -> Int -> Int] -> [Int] -> [Int]
-calibrate fs (x:xs) = aux xs x
+calibrate fs (x : xs) = aux xs x
   where
     aux :: [Int] -> Int -> [Int]
     aux [] total = [total]
-    aux (y:ys) total = concatMap (aux ys) . sequence (sequence fs total) $ y
+    aux (y : ys) total = concatMap (aux ys) . sequence (sequence fs total) $ y
 
 (|||) :: Int -> Int -> Int
 a ||| b = read (show a ++ show b)
 
 part1 :: [(Int, [Int])] -> Int
-part1 = let ops = [(+),(*)]
-        in
-          sum . map fst . filter (uncurry elem) .  map (second (calibrate ops))
+part1 =
+  let ops = [(+), (*)]
+   in sum . map fst . filter (uncurry elem) . map (second (calibrate ops))
 
 part2 :: [(Int, [Int])] -> Int
-part2 = let ops = [(+),(*),(|||)]
-        in sum . map fst . filter (uncurry elem) .  map (second (calibrate ops))
+part2 =
+  let ops = [(+), (*), (|||)]
+   in sum . map fst . filter (uncurry elem) . map (second (calibrate ops))
 
 prepare :: String -> [(Int, [Int])]
-prepare =map ((\[a,b] ->(read a, map read (splitOn " " b))) . splitOn ": ") . lines
+prepare = map ((\[a, b] -> (read a, map read (splitOn " " b))) . splitOn ": ") . lines
 
 main :: IO ()
 main = do
   readFile "small1.txt" >>= print . part1 . prepare
-  -- readFile "small2.txt" >>= print . part2 . prepare
+  readFile "small2.txt" >>= print . part2 . prepare
   readFile "input.txt" >>= print . (part1 &&& part2) . prepare
