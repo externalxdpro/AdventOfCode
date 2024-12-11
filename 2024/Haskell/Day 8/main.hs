674bb7a817fa8dfc438359dedd0a14368a121e01
externalxdpro <externalxdpro@gmail.com>
2024-12-10 22:06:23 -0500

    Add faster and shorter solution

--- 2024/Haskell/Day 8/main.hs
+++ 2024/Haskell/Day 8/main.hs
@@ -4,7 +4,6 @@
 module Main where
 
 import Control.Arrow ((&&&))
-import Data.Bifunctor (second)
 import Data.List (nub, sort, tails)
 import Data.Map (Map)
 import Data.Map qualified as Map
@@ -15,41 +14,40 @@ combinations :: Int -> [a] -> [[a]]
 combinations 0 _ = [[]]
 combinations n xs = [y : ys | y : xs' <- tails xs, ys <- combinations (n - 1) xs']
 
-inside :: Map Coord Char -> Coord -> Bool
+inside :: [(Coord, Char)] -> Coord -> Bool
 inside coords (i, j) =
-  let list = Map.toList coords
-      maxRow = maximum . map (\((i, j), c) -> i) $ list
-      maxCol = maximum . map (\((i, j), c) -> j) $ list
+  let maxRow = maximum . map (\((i, j), c) -> i) $ coords
+      maxCol = maximum . map (\((i, j), c) -> j) $ coords
    in 0 <= i && i <= maxRow && 0 <= j && j <= maxCol
 
-gridToMap :: String -> Map Coord Char
-gridToMap str =
-  Map.fromList
-    [((i, j), c) | (i, r) <- zip [0 ..] (lines str), (j, c) <- zip [0 ..] r]
+gridCoords :: String -> [(Coord, Char)]
+gridCoords str = [((i, j), c) | (i, r) <- zip [0 ..] (lines str), (j, c) <- zip [0 ..] r]
 
-get :: Coord -> Coord -> [Coord]
-get (i1, j1) (i2, j2) =
+getOne :: Coord -> Coord -> [Coord]
+getOne (i1, j1) (i2, j2) =
   let (di, dj) = (i1 - i2, j1 - j2)
    in [(i1 + di, j1 + dj), (i2 - di, j2 - dj)]
 
-getAll :: Coord -> Coord -> [Coord]
-getAll (i1, j1) (i2, j2) =
+getAll :: ([Coord] -> [Coord]) -> Coord -> Coord -> [Coord]
+getAll f (i1, j1) (i2, j2) =
   let (di, dj) = (i1 - i2, j1 - j2)
-   in concat [[(i1 + (di * n), j1 + (dj * n)), (i2 - (di * n), j2 - (dj * n))] | n <- [0 .. 100]]
+      pos = f [(i1 + (di * n), j1 + (dj * n)) | n <- [0 ..]]
+      neg = f [(i2 - (di * n), j2 - (dj * n)) | n <- [0 ..]]
+   in pos ++ neg
 
 part1 :: String -> Int
-part1 str =
-  let cityMap = gridToMap str
-      antennas = nub . concatMap (\[a, b] -> get (fst a) (fst b)) . filter (\[a, b] -> snd a == snd b) . combinations 2 . Map.toList . Map.filter (/= '.') $ cityMap
-      filtered = filter (inside cityMap) antennas
-   in length filtered
+part1 xs =
+  let cityCoords = gridCoords xs
+      antennas = filter (\(_, c) -> c /= '.') cityCoords
+      antinodes = nub . concatMap (\[a, b] -> getOne (fst a) (fst b)) . filter (\[a, b] -> snd a == snd b) . combinations 2 $ antennas
+   in length . filter (inside cityCoords) $ antinodes
 
 part2 :: String -> Int
-part2 str =
-  let cityMap = gridToMap str
-      antennas = nub . filter (inside cityMap) . concatMap (\[a, b] -> getAll (fst a) (fst b)) . filter (\[a, b] -> snd a == snd b) . combinations 2 . Map.toList . Map.filter (/= '.') $ cityMap
-      filtered = filter (inside cityMap) antennas
-   in length filtered
+part2 xs =
+  let cityCoords = gridCoords xs
+      antennas = filter (\(_, c) -> c /= '.') cityCoords
+      antinodes = nub . concatMap (\[a, b] -> getAll (takeWhile (inside cityCoords)) (fst a) (fst b)) . filter (\[a, b] -> snd a == snd b) . combinations 2 $ antennas
+   in length antinodes
 
 main :: IO ()
 main = do
