577f1afcbaa4652a4474ddc856b063cac49238e1
externalxdpro <externalxdpro@gmail.com>
2025-05-23 20:31:11 -0400

    Edit template

--- 2024/Haskell/_Template.hs
+++ 2024/Haskell/_Template.hs
@@ -13,11 +13,7 @@ part1 = const ()
 part2 :: Input -> ()
 part2 = const ()
 
-prepare :: String -> Input
-prepare = lines
-
 main :: IO ()
 main = do
-  readFile "small1.txt" >>= print . part1 . prepare
-  readFile "small2.txt" >>= print . part2 . prepare
-  readFile "input.txt" >>= print . (part1 &&& part2) . prepare
+  readFile "example" >>= print . (part1 &&& part2) . lines
+  readFile "input" >>= print . (part1 &&& part2) . lines
